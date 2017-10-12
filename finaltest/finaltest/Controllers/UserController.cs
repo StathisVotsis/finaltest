3254ce6473d39b182d51dc5b0c73e651ed91876a
Stathis Votsis <stathisvotsis@gmail.com>
2017-10-13 00:18:46 +0300

    h

--- finaltest/finaltest/Controllers/UserController.cs
+++ finaltest/finaltest/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using finaltest.Models;
 
 namespace finaltest.Controllers
 {
+    
     public class UserController : Controller
     {
         [HttpGet]
@@ -19,7 +20,34 @@ namespace finaltest.Controllers
         [HttpPost]
         public ActionResult Registration(User userModel)
         {
+            using (DbModels dbModel = new DbModels())
+            {
+                if(dbModel.Users.Any(x =>x.Username == userModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "User already exists";
+                    return View("Registration", new User());
+                }
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "You have registered as" +" "+userModel.Username;
+            return View("Registration", new User());
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(User userModel)
+        {
+            return View();
+        }
+
+
     }
 }
