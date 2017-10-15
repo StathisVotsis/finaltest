27e942c821f8d3a4a3251d7e6803692726f6d826
Stathis Votsis <stathisvotsis@gmail.com>
2017-10-15 21:59:00 +0300

    qwe

--- finaltest/finaltest/Controllers/HomeController.cs
+++ finaltest/finaltest/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Particle.SDK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,8 +12,23 @@ namespace finaltest.Controllers
     public class HomeController : Controller
     {
         [Authorize(Roles = "Admin")]
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            var success = await ParticleCloud.SharedCloud.LoginAsync("stathisvotsis@gmail.com", "eystbots");
+            ParticleDevice myDevice = null;
+            List<ParticleDevice> devices = await ParticleCloud.SharedCloud.GetDevicesAsync();
+            foreach (ParticleDevice device in devices)
+            {
+                //MessageBox.Show(device.Name.ToString());
+                myDevice = device;
+            }
+            ViewBag.PhotonMessage = "You device is" + " " + myDevice.Name.ToString();
+            foreach (string functionName in myDevice.Functions)
+                ViewBag.Photon2Message = ($"Device has function: {functionName}");
+            var functionResponse = await myDevice.RunFunctionAsync("relayOn", "2");
+            var result = functionResponse.ReturnValue;
+           
+            //ViewBag.Photon3Message(result.ToString());
             return View();
         }
 
